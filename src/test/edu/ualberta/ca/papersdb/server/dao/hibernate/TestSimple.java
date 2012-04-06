e80a4769e529ebb7adbe4356de09d54788e253d8
Nelson Loyola <nloyola@gmail.com>
2012-04-05 23:11:57 -0600

    added spring integration tests

---
+++ src/test/edu/ualberta/ca/papersdb/server/dao/hibernate/TestSimple.java
@@ -0,0 +1,21 @@
+package test.edu.ualberta.ca.papersdb.server.dao.hibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@TransactionConfiguration
+@Transactional
+public class TestSimple {
+
+    @Test
+    public void testCreate() {
+
+        assert (true);
+    }
+}
